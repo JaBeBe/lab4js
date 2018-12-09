f05a453904ca178a2b33fc2be5c0d626205a1689
Jacek Bebak <jacek.bebak@microsoft.wsei.edu.pl>
2018-12-09 20:16:56 +0100

    beautify

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', appStart)
 
-const sound = 
-{
-    97 :"boom",
-    115:"clap",
-    100:"hihat",
-    102:"kick",
-    103:"openhat",
-    104:"ride",
-    106:"snare",
-    107:"tink",
-    108:"tom",
+const sound = {
+    97: "boom",
+    115: "clap",
+    100: "hihat",
+    102: "kick",
+    103: "openhat",
+    104: "ride",
+    106: "snare",
+    107: "tink",
+    108: "tom",
 }
 
 const chanel1 = []
@@ -20,55 +19,47 @@ const chanel4 = []
 
 let isRec = false
 let recStartTime = 0
-function appStart()
-{
+
+function appStart() {
     window.addEventListener('keypress', playSound)
     document.querySelector('#rec').addEventListener('click', recAudio)
     document.querySelector('#play').addEventListener('click', playAudio)
 }
 
-function playSound(e)
-{
-    if (!sound[e.charCode]) 
-    {
+function playSound(e) {
+    if (!sound[e.charCode]) {
         return
     }
     const soundName = sound[e.charCode]
     const audioDOM = document.querySelector(`#${soundName}`)
     audioDOM.currentTime = 0
     audioDOM.play()
-    
-    if (isRec) 
-    {
-       chanel1.push(
-        {
-            name:soundName,
-            time:Date.now()-recStartTime
-        } 
-       ) 
+
+    if (isRec) {
+        chanel1.push({
+            name: soundName,
+            time: Date.now() - recStartTime
+        })
     }
-   
+
     // document.body.innerHTML += e.charCode + '<br>'
 }
 
-function recAudio(e)
-{
+function recAudio(e) {
     isRec = !isRec
     recStartTime = Date.now()
 
-    e.target.innerHTML = isRec? "Zatrzymaj":"Nagrywaj"
+    e.target.innerHTML = isRec ? "Zatrzymaj" : "Nagrywaj"
 }
-function playAudio() 
-{
-    chanel1.forEach(sound =>
-        {
-            setTimeout(
-                ()=>
-                {
-                    const audioDOM = document.querySelector(`#${sound.name}`)
-                    audioDOM.currentTime = 0
-                    audioDOM.play()
-                },sound.time
-            )
-        })
+
+function playAudio() {
+    chanel1.forEach(sound => {
+        setTimeout(
+            () => {
+                const audioDOM = document.querySelector(`#${sound.name}`)
+                audioDOM.currentTime = 0
+                audioDOM.play()
+            }, sound.time
+        )
+    })
 }
